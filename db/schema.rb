d70f97ed0c54c6de766b41c2a8ab720dbba81af2
Kenya <kenya0324@gmail.com>
2019-12-09 11:53:32 +0000

    modelu

--- db/schema.rb
+++ db/schema.rb
@@ -13,19 +13,19 @@
 ActiveRecord::Schema.define(version: 2019_12_09_110008) do
 
   create_table "artists", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "labels", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
