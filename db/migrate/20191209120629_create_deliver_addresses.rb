c1eb3e259b23e4bdc02e507e3e0192cde46ba2c5
fujiraidar <fujirider@icloud.com>
2019-12-10 04:53:29 +0000

    fuji

---
+++ db/migrate/20191209120629_create_deliver_addresses.rb
@@ -0,0 +1,16 @@
+class CreateDeliverAddresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :deliver_addresses do |t|
+    	t.integer :user_id, :null => false
+    	t.string :deliver_post_front, :null => false
+    	t.string :deliver_post_back, :null => false
+    	t.string :deliver_prefecture, :null => false
+    	t.text :deliver_town, :null => false
+    	t.integer :deliver_post_namber, :null => false
+    	t.string :deliver_condo
+
+
+      t.timestamps
+    end
+  end
+end
