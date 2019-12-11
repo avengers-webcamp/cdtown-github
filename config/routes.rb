8ff6671041cefa0c69ead66b83d1527e42225dbb
Kenya <kenya0324@gmail.com>
2019-12-11 06:30:04 +0000

    r

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,22 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
   sessions: 'users/sessions'
 }
+  resources :users do
+  	resources :deliver_addresses
+  end
 
+  resources :cds do
+  	resources :discs do
+  end
+  		resources :songs
+  end
+  resources :orders
+  resources :owners
+  resources :arrivals
+  resources :artists
+  resources :genres
+  resources :labels
+  resources :user_cds
+  resources :cd_orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
