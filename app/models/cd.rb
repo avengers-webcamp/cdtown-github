0e6723a6c56ed571ec80efb17a6da07963a6f317
Kenya <kenya0324@gmail.com>
2019-12-11 07:05:30 +0000

    h

--- app/models/cd.rb
+++ app/models/cd.rb
@@ -1,2 +1,11 @@
 class Cd < ApplicationRecord
+	belongs_to :artist
+	belongs_to :genre
+	belongs_to :label
+	belongs_to :arrival
+	has_many :discs
+	has_many :user_cds
+	has_many :users, throuth: :user_cds, source: :user
+	has_many :cd_orders
+	has_many :orders, throuth: :cd_orders, source: :order
 end
