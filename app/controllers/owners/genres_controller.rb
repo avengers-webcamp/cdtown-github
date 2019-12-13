f09589fa1c8dbd6c7c463c7f328518a58be584fd
Kenya <kenya0324@gmail.com>
2019-12-13 09:00:21 +0000

    artist_genre_label

--- app/controllers/owners/genres_controller.rb
+++ app/controllers/owners/genres_controller.rb
@@ -1,5 +1,20 @@
 class Owners::GenresController < ApplicationController
+	def new
+		@genre = Genre.new
+	end
 
 	def create
+        @genre = Genre.new(genre_params)
+      if  @genre.save
+          redirect_back(fallback_location: root_path)
+      else
+          redirect_back(fallback_location: root_path)
+      end
 	end
+
+	private
+    def genre_params
+        params.require(:genre).permit(:name)
+    end
+
 end
