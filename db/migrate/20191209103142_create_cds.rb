b4ce4db8963fbc3a590a575ab742e530ff5865f7
naoto0466 <traineve66@gmail.com>
2019-12-14 11:36:54 +0000

    new index ok

--- db/migrate/20191209103142_create_cds.rb
+++ db/migrate/20191209103142_create_cds.rb
@@ -2,15 +2,14 @@ class CreateCds < ActiveRecord::Migration[5.2]
   def change
     create_table :cds do |t|
      t.string :name, :null => false
-     t.integer :user_id, :null => false
      t.integer :artist_id, :null => false
      t.integer :genre_id, :null => false
      t.integer :label_id, :null => false
-     t.integer :arrive_id, :null => false
+     t.integer :arrive_id
      t.string :jacket_image_id
      t.datetime :released_at, :null => false
      t.string :price, :null => false
-     t.integer :stock, :null => false
+     t.integer :stock
      t.integer :status, :null => false
      t.datetime :deleted_at
       t.timestamps
