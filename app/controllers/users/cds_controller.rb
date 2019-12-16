0b3a6118b3d3398efd47803707ea055117b55816
naoto0466 <traineve66@gmail.com>
2019-12-16 06:31:25 +0000

    add users index show

--- app/controllers/users/cds_controller.rb
+++ app/controllers/users/cds_controller.rb
@@ -1,8 +1,20 @@
 class Users::CdsController < ApplicationController
 
 	def show
+		@cd = Cd.find(params[:id])
+		@artists = Artist.select("name")
+		@genres = Genre.select("name")
+		@labels = Label.select("name")
 	end
 
 	def index
+	    @cd = Cd.new
+		@cds = Cd.all
 	end
+
+	private
+    def cd_params
+    params.require(:cd).permit(:name, :jacket_image, :status, :price, :artist_id, :released_at, :label_id, :genre_id, discs_attributes: [:id, :disc_count, :description, :done, :_destroy,
+                                                         songs_attributes: [:id, :name, :order, :description, :_destroy]])
+    end
 end
