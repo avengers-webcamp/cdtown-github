7eca9068c3dfd7ef024fc277656e622dd2ec4495
Kenya <kenya0324@gmail.com>
2019-12-16 07:39:54 +0000

    owner

--- app/controllers/owners/users_controller.rb
+++ app/controllers/owners/users_controller.rb
@@ -1,15 +1,27 @@
 class Owners::UsersController < ApplicationController
 
-	def show
+	def index
+		@users = User.all
 	end
 
-	def index
+	def show
+		@user = User.find(params[:id])
 	end
 
 	def edit
+		@user = User.find(params[:id])
 	end
 
 	def update
+		@user = User.find(params[:id])
+        @user.update(user_params)
+        redirect_to owners_user_path(@user.id)
 	end
+
+	private
+    def user_params
+        params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana,
+        	:post_front, :post_back, :prefecture, :town, :post_number, :condo, :phone_number)
+    end
 
 end
