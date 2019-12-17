8efc68f177c0364345fb502962cc5212f029eb02
fujiraidar <fujirider@icloud.com>
2019-12-17 05:40:16 +0000

    fuji

--- app/controllers/users/user_cds_controller.rb
+++ app/controllers/users/user_cds_controller.rb
@@ -1,16 +1,33 @@
 class Users::UserCdsController < ApplicationController
 
 	def index
-		@user_cds = UserCd.all
+		@user = current_user.id
+		@cart = current_user_cd
 	end
 
 	def create
-		user_cd = UserCd.new(user_cd_params)
-		user_cd.save
-		session[:user_cd] = [] unless session[:user_cd]
-        session[:user_cd] << params[:cd_id]
-        flash[:success] = "カートに追加できました！"
-        redirect_to root_path
+        cd = Cd.find(params[:cd_id])
+        @cart = current_user_cd
+		cart = UserCd.new(user_cd_params)
+		cart.user_id = current_user.id
+        cart.cd_id = cd.id
+
+
+		puts"-------1-------"
+		puts params[:cd_id]
+		puts"-------2-------"
+		puts params[:user_id]
+		puts"-------3-------"
+
+
+		if cart.save!
+		    session[:user_cd] = [] unless session[:user_cd]
+            session[:user_cd] << params[:cd_id]
+            flash[:success] = "カートに追加できました！"
+            redirect_to user_cds_path
+        else
+            redirect_to root_path
+        end
 	end
 
 	def update
@@ -22,7 +39,7 @@ class Users::UserCdsController < ApplicationController
 	private
 
 	def user_cd_params
-		params.require(:user_cd).permit(:user_id, :cd_id)
+		params.require(:user_cd).permit(:user_id, :cd_id ,:disc_count)
 	end
 
 end
