0cf6129f354445e09fac1e6b731859f0c572927c
Kenya <kenya0324@gmail.com>
2019-12-17 10:11:17 +0000

    cart

--- app/controllers/users/user_cds_controller.rb
+++ app/controllers/users/user_cds_controller.rb
@@ -1,29 +1,43 @@
 class Users::UserCdsController < ApplicationController
 
 	def index
-		@user = current_user.id
 		@cart = UserCd.all
 	end
 
+	def show
+	  	@user = User.find(params[:id])
+	  	@cart = UserCd.all
+    end
+
 	def create
         cd = Cd.find(params[:cd_id])
-        @cart = current_user_cd
 		cart = UserCd.new(user_cd_params)
 		cart.user_id = current_user.id
         cart.cd_id = cd.id
 
 		if cart.save!
-            flash[:success] = "カートに追加できました！"
-            redirect_to user_cds_path
+            flash[:notice] = "カートに追加できました！"
+            redirect_to user_cd_path(current_user.id)
         else
             redirect_to root_path
         end
 	end
 
 	def update
+		cart = UserCd.find(params[:id])
+
+        if  cart.update(user_cd_params)
+            flash[:notice] = "successfully"
+            redirect_to user_cd_path(current_user.id)
+        else
+	        redirect_to root_path
+        end
 	end
 
 	def destroy
+		cart = UserCd.find(params[:id])
+		cart.destroy
+        redirect_to user_cd_path(current_user.id)
 	end
 
 	private
