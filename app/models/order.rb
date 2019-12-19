4b74f4132870fa7d0073855f6233a99ffddb05c8
naoto0466 <traineve66@gmail.com>
2019-12-19 12:35:32 +0000

    add validete   p

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,13 @@ class Order < ApplicationRecord
 	belongs_to :user
 	has_many :cd_orders
 	has_many :cds, through: :cd_orders, source: :cd
+
+	enum status: {
+		発送中: 0,
+		発送済み: 1
+		 }
+
+   validates :user_id, presence: true
+   validates :shipping_day, presence: true
+   validates :payment, presence: true
 end
